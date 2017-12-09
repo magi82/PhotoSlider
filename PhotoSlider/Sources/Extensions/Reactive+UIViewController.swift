3a67665eb3250a0d8bc14b3bb7e7d8e3be0cddd6
DoohoDoo <doohochang@gmail.com>
2017-12-09 14:17:31 +0900

    Implement UIViewController Reactive extension

--- PhotoSlider/Sources/Extensions/Reactive+UIViewController.swift
+++ PhotoSlider/Sources/Extensions/Reactive+UIViewController.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2017년 Dooho Chang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+extension Reactive where Base: UIViewController {
+    var viewDidAppear: ControlEvent<Void> {
+        let signal = methodInvoked(#selector(Base.viewWillAppear))
+            .map { _ in Void() }
+            .takeUntil(deallocated)
+        
+        return ControlEvent(events: signal)
+    }
+}
+
